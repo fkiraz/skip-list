569d223e8070b9ee844c7ab4bba0c9347465f364
Vladimir Petrigo <taenaru@gmail.com>
2015-07-28 13:24:46 +0300

    Change the example

--- sklist/Source.cpp
+++ sklist/Source.cpp
@@ -4,19 +4,48 @@
 using namespace std;
 using namespace List_h;
 
+void foo() {
+    Skip_list ll;
+
+    for (auto i = -50; i < 50000; ++i) {
+        ll.push_back(i);
+    }
+
+    ll.display();
+}
+
 int main() {
-    Slink_list sl{ 1, 2, 3, 4, 5 };
-    Slink_list tl;
+    Skip_list sl;
+    
+    sl.push_back(3);
+    sl.push_back(6);
+    sl.push_back(7);
+    sl.push_back(9);
+    sl.push_back(12);
+    sl.push_back(17);
+    sl.push_back(19);
+    sl.push_back(25);
+    sl.push_back(26);
 
-    const Link *list = sl.get_head();
+    sl.insert(40, 40);
+    sl.push_front(1);
 
-    print_link(tl.get_head());
+    sl.delete_node(1);
+    sl.delete_node(17);
 
-    sl.push_back(nullptr);
-    tl.push_back(new Link{ 120 });
+    sl.display();
 
-    print_link(list);
-    print_link(tl.get_head());
+    int val;
+
+    std::cout << "Please enter a number to search: ";
+    std::cin >> val;
+
+    if (sl.search(val)) {
+        std::cout << val << " was found" << std::endl;
+    }
+    else {
+        std::cout << val << " was not found" << std::endl;
+    }
 
     return 0;
 }
